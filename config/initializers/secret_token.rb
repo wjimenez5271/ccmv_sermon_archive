4ef1f38a6be7f615c9748ff8c7be67901a52b210
Daniel Imfeld <daniel@danielimfeld.com>
2012-01-23 18:37:53 -0800

    Skeleton project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CcmvSermonArchive::Application.config.secret_token = 'c010cf7af7bcc1f128aed7a4664249544ae4f2f3e91d7698a6f43c702df71638f581df9ea6790e74bc9e8cacceea03de6c27b4791cb5505a373c58b6441c89e6'
